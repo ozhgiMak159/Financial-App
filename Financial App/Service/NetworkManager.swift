7b0617b4ec068b75f9eb17f7fa9f79defdaba38e
Maksim Ozhgibecov <93679282+ozhgiMak159@users.noreply.github.com>
2022-08-04 11:24:01 +0500

    Network

---
+++ Financial App/Service/NetworkManager.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkManager.swift
+//  Financial App
+//
+//  Created by Maksim  on 04.08.2022.
+//
+
+import Foundation
+
+enum API: String {
+    case apiKey = "FC6IT3JSCG8DUE1X"
+    case apiKey2 = "EYZ54UFGZMM4ADIY"
+    case apiKey3 = "T9L1HBB7B26SKHHX"
+}
---
+++ Financial App/View/SearchTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  SearchTableViewCell.swift
+//  Financial App
+//
+//  Created by Maksim  on 04.08.2022.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var assetNameLabel: UILabel!
+    @IBOutlet weak var assetSymbolLabel: UILabel!
+    @IBOutlet weak var assetTypeLabel: UILabel!
+    
+
+}
